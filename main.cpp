8caf2da0f85b5085df9464e27fee1d3908ead8c5
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2024-12-08 07:07:59 +0900

    1.0.9

--- main.cpp
+++ main.cpp
@@ -266,7 +266,7 @@ std::string normalDump(AnalyzeData data) {
     return ss.str();
 }
 
-const std::string version = "v1.0.8";
+const std::string version = "v1.0.9";
 //int main(int argc, char *argv[]) {
 int main() {
 #ifdef BUILD_DATE
