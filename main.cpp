7041f65fb891f42b1f9bb1001de1c54c18f4d2c2
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-04-17 14:54:47 +0900

    病魔: v5.0.4

--- main.cpp
+++ main.cpp
@@ -61,7 +61,7 @@ namespace {
 
     uint64_t FoundSeed = 0;
 
-    const char *version = "v5.0.3";
+    const char *version = "v5.0.4";
 
     std::stringstream performanceLogger = std::stringstream();
 
