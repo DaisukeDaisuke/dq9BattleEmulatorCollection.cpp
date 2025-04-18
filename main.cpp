8a9af54ede3ed101252ea219d53cdf218c379c7c
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-04-18 22:26:43 +0900

    ジャダーマ: v6.0.4

--- main.cpp
+++ main.cpp
@@ -64,7 +64,7 @@ namespace {
 
     uint64_t FoundSeed = 0;
 
-    const char *version = "v6.0.3";
+    const char *version = "v6.0.4";
 
     std::stringstream performanceLogger = std::stringstream();
 
