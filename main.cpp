638775b3fcb5e1834ac2c0bfc307530d2a412bcd
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-04-29 01:47:45 +0900

    erugiosu2: 変数名修正とバージョン更新

--- main.cpp
+++ main.cpp
@@ -266,7 +266,7 @@ std::string normalDump(AnalyzeData data) {
     return ss.str();
 }
 
-const std::string version = "v1.0.15";
+const std::string version = "v1.0.16";
 void showHeader() {
 #ifdef BUILD_DATE
     const std::string buildDate = BUILD_DATE;
