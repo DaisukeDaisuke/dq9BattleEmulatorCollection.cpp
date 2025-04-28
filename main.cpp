f226e8299b2a76bd9db66b95515e64e74acd7fc2
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-04-29 01:48:45 +0900

    yo2_v4: 変数名の修正とバージョン情報の更新

--- main.cpp
+++ main.cpp
@@ -60,7 +60,7 @@ namespace {
 
     uint64_t FoundSeed = 0;
 
-    const char *version = "v4.0.3_vZ_aa";
+    const char *version = "v4.0.3_vG_aa";
 
     std::stringstream performanceLogger = std::stringstream();
 
