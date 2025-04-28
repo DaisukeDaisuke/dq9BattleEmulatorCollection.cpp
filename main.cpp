1edde9b2fa5a0b00cfc0c8be75a4332675fe2a46
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-04-29 01:09:11 +0900

    yo2_v4: バグ回避条件追加およびバージョン情報更新

--- main.cpp
+++ main.cpp
@@ -60,7 +60,7 @@ namespace {
 
     uint64_t FoundSeed = 0;
 
-    const char *version = "v4.0.3_vU_aa";
+    const char *version = "v4.0.3_vZ_aa";
 
     std::stringstream performanceLogger = std::stringstream();
 
