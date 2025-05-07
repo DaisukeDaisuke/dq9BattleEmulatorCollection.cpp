0b048823234515a70751e39c917a17097f0e662e
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-05-08 01:04:45 +0900

    zilyamama: バージョン文字列を更新

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ namespace {
 
     uint64_t FoundSeed = 0;
 
-    const char *version = "v6.0.4_vS_aa";
+    const char *version = "v6.0.5_vF_v2";
 
     std::stringstream performanceLogger = std::stringstream();
 
