34d5a86b1cbb4a9889884e9ee88e01d83ed8f984
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-05-08 20:10:11 +0900

    unknown: バージョン情報を更新
    コード中のバージョン文字列を "v5.0.6_vG_v2" に変更。

--- main.cpp
+++ main.cpp
@@ -63,7 +63,7 @@ namespace {
     int foundTurn = 0;
     int foundTurnOffset = 0;
 
-    const char *version = "v5.0.6_vF_v2";
+    const char *version = "v5.0.6_vG_v2";
 
     std::stringstream performanceLogger = std::stringstream();
 
