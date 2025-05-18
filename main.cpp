e556a28d159b6c58801a0014efcb2b760bbfb422
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2025-05-18 17:16:58 +0900

    erugiosu_ten: 実行時間測定コードの削除 - #ifdef DEBUGブロック内の実行時間を測定するコードを削除

--- main.cpp
+++ main.cpp
@@ -274,10 +274,6 @@ void showHeader() {
 //int main(int argc, char *argv[]) {
 int main() {
     showHeader();
-#ifdef DEBUG
-    auto t0 = std::chrono::high_resolution_clock::now();
-#endif
-
 
     //https://zenn.dev/reputeless/books/standard-cpp-for-competitive-programming/viewer/library-ios-iomanip#3.1-c-%E8%A8%80%E8%AA%9E%E3%81%AE%E5%85%A5%E5%87%BA%E5%8A%9B%E3%82%B9%E3%83%88%E3%83%AA%E3%83%BC%E3%83%A0%E3%81%A8%E3%81%AE%E5%90%8C%E6%9C%9F%E3%82%92%E7%84%A1%E5%8A%B9%E3%81%AB%E3%81%99%E3%82%8B
     //std::cin.tie(0)->sync_with_stdio(0);
@@ -458,13 +454,6 @@ int main() {
 
     mainLoop(copiedPlayers);
     return 0;
-
-#ifdef DEBUG
-    auto t1 = std::chrono::high_resolution_clock::now();
-    auto elapsed_time =
-            std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
-    std::cout << "elapsed time: " << double(elapsed_time) / 1000 << " ms" << std::endl;
-#endif
     return 0;
 }
 
